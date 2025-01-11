fbe09d6951089d5a46c6ac31db1d42d82e55290c
Erich Hafenmaier <ehafenmaier@gmail.com>
2025-01-11 14:17:32 -0800

    Add helper function to get aspect ratio of an uploaded video file

--- helpers.go
+++ helpers.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
-	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"math"
+	"os/exec"
 )
 
 func createRandomFileName() string {
@@ -12,7 +16,7 @@ func createRandomFileName() string {
 		return ""
 	}
 
-	return base64.URLEncoding.EncodeToString(b)
+	return hex.EncodeToString(b)
 }
 
 func getThumbnailFileExtension(mediaType string) string {
@@ -23,5 +27,39 @@ func getThumbnailFileExtension(mediaType string) string {
 		return "png"
 	default:
 		return "jpg"
+	}
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	// Build and run the ffprobe command
+	var b bytes.Buffer
+	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
+	cmd.Stdout = &b
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	// Unmarshal the JSON output
+	var output map[string]interface{}
+	err = json.Unmarshal(b.Bytes(), &output)
+	if err != nil {
+		return "", err
+	}
+
+	// Calculate the aspect ratio from the width and height
+	stream := output["streams"].([]interface{})[0].(map[string]interface{})
+	width := stream["width"].(float64)
+	height := stream["height"].(float64)
+	ratio := math.Round((width/height)*100) / 100
+
+	// Return the aspect ratio as a string
+	switch ratio {
+	case 1.78:
+		return "16:9", nil
+	case 0.56:
+		return "9:16", nil
+	default:
+		return "other", nil
 	}
 }
